da97a6733c803e550d6885f3f6699c6a56170d1b
Cory G <me@coryg.io>
2016-10-07 07:41:57 -0500

    minor changes

--- js/showcard.js
+++ js/showcard.js
@@ -1,7 +1,10 @@
 function showCard() {
    document.getElementById('card').style.display = "block";
+   document.getElementById('show-card').style.display = "none";
+   document.getElementById('show-card').style.display = "";
 }
 
 function hideCard() {
    document.getElementById('card').style.display = "none";
+   document.getElementById('show-card').style.display = "block";
 }
