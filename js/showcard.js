c240ba25ada14b79d420c7afafbd467bf31f6a18
Cory G <me@coryg.io>
2016-10-07 15:22:36 -0500

    Broke things, Little progress

--- js/showcard.js
+++ js/showcard.js
@@ -1,10 +1,13 @@
 function showCard() {
-   document.getElementById('card').style.display = "block";
+   document.getElementById('card-container').style.display = "block";
    document.getElementById('show-card').style.display = "none";
-   document.getElementById('show-card').style.display = "";
+   document.getElementById('bg-blur').style.display = "block";
+
 }
 
 function hideCard() {
-   document.getElementById('card').style.display = "none";
+   document.getElementById('card-container').style.display = "none";
    document.getElementById('show-card').style.display = "block";
+   document.getElementById('bg-blur').style.display = "none";
+
 }
