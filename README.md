44d5a0d651596555da53440ffc74fdc92007a004
Cory G <me@coryg.io>
2017-07-19 06:32:24 -0500

    beginning of vue app development

--- README.md
+++ README.md
@@ -1,8 +1,21 @@
-## jem-site
+# jem-site
 
-This page has been created as a gift for my astronomy enthusiast girlfriend. Essentially it is a clone of [NASA's Astronomy Picture of the Day](https://apod.nasa.gov/apod/astropix.html "NASA AOPD") that has been given the material treatment. It uses The [Materialize CSS](http://materializecss.com/) Framework, [NASA Open API](https://api.nasa.gov/api.html), and is hosted here at gh-pages.
+> A clone (functionally) of NASA's Astronomy Picture of the Day created as a gift for my astronomy enthusiast girlfriend.
 
-####Languages
-* Jade
-* Sass
-* CoffeeScript
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template lang="pug" src="@/templates/app.pug">
+
+</template>
+
+<script>
+  import Vue from 'vue'
+  import axios from 'axios'
+  import VueAxios from 'vue-axios'
+
+  Vue.use(VueAxios, axios)
+
+  var nasaApiUrl = 'https://api.nasa.gov/planetary/apod?api_key='
+  var nasaApiKey = 'uI5nmso1mWCHxbGyzVqecAYD07O8j5QGsE8tUgVw'
+  var nasaApi = nasaApiUrl + nasaApiKey
+
+  export default {
+    data () {
+      return {
+        navItems: [
+          {
+            icon: 'star',
+            title: 'Yeah',
+            url: 'http://nasa.gov'
+          },
+          {
+            icon: 'dashboard',
+            title: 'Okay',
+            url: 'http://google.com'
+          }
+        ],
+        sideNav: false,
+        apod: [],
+        fab: {
+          show: false,
+          transition: 'slide-x-reverse-transition',
+          direction: 'top'
+        }
+      }
+    },
+    created () {
+      this.$http.get(nasaApi).then((response) => {
+        console.log(response.data)
+        this.apod = response.data
+      })
+    }
+  }
+</script>
+
+<style lang="stylus">
+  @import './stylus/main'
+
+  #app .speed-dial {
+    position: absolute;
+  }
+
+  #app .btn--floating {
+    position: relative;
+  }
+</style>
